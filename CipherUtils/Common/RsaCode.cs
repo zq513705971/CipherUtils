c26e3e0f7717c5acdbb37a7f3723f36a13ba075f
zhanqun <513705971@qq.com>
2015-03-03 17:43:47 +0800

    Add AES

--- CipherUtils/Common/RsaCode.cs
+++ CipherUtils/Common/RsaCode.cs
@@ -526,4 +526,284 @@ namespace IBS.Data
             }
         }
     }
+
+    public class AESCode
+    {
+        /// <summary>   
+        /// 加密方法 
+        /// </summary> 
+        /// <param name="encryptVal">需要加密字符串</param>
+        /// <param name="key">密钥</param>
+        /// <returns>加密后的字符串</returns>
+        public static string AESEncrypt(string encryptVal, string key)
+        {
+            if (string.IsNullOrEmpty(encryptVal))
+                return null;
+#if CSP
+            using (AesCryptoServiceProvider des = new AesCryptoServiceProvider())
+#else
+            using (AesManaged des = new AesManaged())
+#endif
+            {
+                byte[] inputByteArray = Encoding.UTF8.GetBytes(encryptVal);
+                des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inputByteArray, 0, inputByteArray.Length);
+                        cs.FlushFinalBlock();
+                        byte[] bytes = (byte[])ms.ToArray();
+                        return Convert.ToBase64String(bytes, 0, bytes.Length);
+                    }
+                }
+            }
+        }
+
+        /// <summary>   
+        /// 解密方法  
+        /// </summary>  
+        /// <param name="decryptVal">需要解密的字符串</param>   
+        /// <param name="key">密匙</param>  
+        /// <returns>解密后的字符串</returns> 
+        public static string AESDecrypt(string decryptVal, string key)
+        {
+            if (string.IsNullOrEmpty(decryptVal))
+                return null;
+#if CSP
+            using (AesCryptoServiceProvider des = new AesCryptoServiceProvider())
+#else
+            using (AesManaged des = new AesManaged())
+#endif
+            {
+                byte[] inputByteArray = Convert.FromBase64String(decryptVal);
+                des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inputByteArray, 0, inputByteArray.Length);
+                        cs.FlushFinalBlock();
+                        return Encoding.UTF8.GetString(ms.ToArray());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="publickey"></param>
+        /// <param name="dt"></param>
+        /// <param name="columnIndexs"></param>
+        /// <returns></returns>
+        public static DataTable AESEncrypt(string key, DataTable dt, int[] columnIndexs)
+        {
+            if (dt == null)
+                return null;
+            DataTable result = dt.Clone();
+#if CSP
+            using (AesCryptoServiceProvider des = new AesCryptoServiceProvider())
+#else
+            using (AesManaged des = new AesManaged())
+#endif
+            {
+                des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+                ICryptoTransform transform = des.CreateEncryptor();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    object[] objs = dr.ItemArray;
+                    foreach (int index in columnIndexs)
+                    {
+                        if (objs[index] != null && objs[index] != DBNull.Value)
+                        {
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                using (CryptoStream cs = new CryptoStream(ms, transform, CryptoStreamMode.Write))
+                                {
+                                    byte[] src = Encoding.UTF8.GetBytes(objs[index].ToString());
+                                    if (src.Length == 0)
+                                        continue;
+                                    cs.Write(src, 0, src.Length);
+                                    cs.FlushFinalBlock();
+                                    byte[] bytes = (byte[])ms.ToArray();
+                                    objs[index] = Convert.ToBase64String(bytes);
+                                }
+                            }
+                        }
+                    }
+                    result.Rows.Add(objs);
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="publickey"></param>
+        /// <param name="dt"></param>
+        /// <param name="columnIndexs"></param>
+        /// <returns></returns>
+        public static DataTable AESDecrypt(string key, DataTable dt, int[] columnIndexs)
+        {
+            if (dt == null)
+                return null;
+            DataTable result = dt.Clone();
+#if CSP
+            using (AesCryptoServiceProvider des = new AesCryptoServiceProvider())
+#else
+            using (AesManaged des = new AesManaged())
+#endif
+            {
+                des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+                ICryptoTransform transform = des.CreateDecryptor();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    object[] objs = dr.ItemArray;
+                    foreach (int index in columnIndexs)
+                    {
+                        if (objs[index] != null && objs[index] != DBNull.Value)
+                        {
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                using (CryptoStream cs = new CryptoStream(ms, transform, CryptoStreamMode.Write))
+                                {
+                                    byte[] src = Convert.FromBase64String(objs[index].ToString());
+                                    if (src.Length == 0)
+                                        continue;
+                                    cs.Write(src, 0, src.Length);
+                                    cs.FlushFinalBlock();
+                                    byte[] bytes = (byte[])ms.ToArray();
+                                    objs[index] = Encoding.UTF8.GetString(ms.ToArray());
+                                }
+                            }
+                        }
+                    }
+                    result.Rows.Add(objs);
+                }
+            }
+            return result;
+        }
+
+        public static int AESEncrypt(string key, string src, string dest, int[] columns, Predicate<string> action)
+        {
+#if CSP
+            using (AesCryptoServiceProvider des = new AesCryptoServiceProvider())
+#else
+            using (AesManaged des = new AesManaged())
+#endif
+            {
+                des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+                ICryptoTransform transform = des.CreateEncryptor();
+                using (TextReader reader = new StreamReader(src, Encoding.Default))
+                {
+                    using (TextWriter writer = new StreamWriter(dest, false, Encoding.Default))
+                    {
+                        CsvReader _reader = new CsvReader(reader, true);
+                        int rowIndex = 0;
+                        writer.WriteLine(string.Join(",", _reader.GetFieldHeaders()));
+                        while (_reader.ReadNextRecord())
+                        {
+                            if (rowIndex > 0 && rowIndex % 100 == 0 && action != null)
+                            {
+                                if (!action(string.Format("正在处理第{0}行...", rowIndex)))
+                                    break;
+                            }
+                            string[] objs = new string[_reader.FieldCount];
+                            for (int index = 0; index < objs.Length; index++)
+                            {
+                                if (_reader[index] != null && Array.Exists(columns, (column) => { return Convert.ToInt32(column) == index; }))
+                                {
+                                    using (MemoryStream ms = new MemoryStream())
+                                    {
+                                        using (CryptoStream cs = new CryptoStream(ms, transform, CryptoStreamMode.Write))
+                                        {
+                                            byte[] _bytes = Encoding.UTF8.GetBytes(_reader[index].ToString());
+                                            if (_bytes.Length == 0)
+                                                continue;
+                                            cs.Write(_bytes, 0, _bytes.Length);
+                                            cs.FlushFinalBlock();
+                                            byte[] bytes = (byte[])ms.ToArray();
+                                            objs[index] = Convert.ToBase64String(bytes);
+                                        }
+                                    }
+                                }
+                                else
+                                    objs[index] = _reader[index];
+                            }
+                            writer.WriteLine(string.Join(",", objs));
+                            rowIndex++;
+                        }
+                        reader.Close();
+                        writer.Close();
+                        return rowIndex;
+                    }
+                }
+            }
+        }
+
+        public static int AESDecrypt(string key, string src, string dest, int[] columns, Predicate<string> action)
+        {
+#if CSP
+            using (AesCryptoServiceProvider des = new AesCryptoServiceProvider())
+#else
+            using (AesManaged des = new AesManaged())
+#endif
+            {
+                des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+                ICryptoTransform transform = des.CreateDecryptor();
+                using (TextReader reader = new StreamReader(src, Encoding.Default))
+                {
+                    using (TextWriter writer = new StreamWriter(dest, false, Encoding.Default))
+                    {
+                        CsvReader _reader = new CsvReader(reader, true);
+                        int rowIndex = 0;
+                        writer.WriteLine(string.Join(",", _reader.GetFieldHeaders()));
+                        while (_reader.ReadNextRecord())
+                        {
+                            if (rowIndex > 0 && rowIndex % 100 == 0 && action != null)
+                            {
+                                if (!action(string.Format("正在处理第{0}行...", rowIndex)))
+                                    break;
+                            }
+                            string[] objs = new string[_reader.FieldCount];
+                            for (int index = 0; index < objs.Length; index++)
+                            {
+                                if (_reader[index] != null && Array.Exists(columns, (column) => { return Convert.ToInt32(column) == index; }))
+                                {
+                                    using (MemoryStream ms = new MemoryStream())
+                                    {
+                                        using (CryptoStream cs = new CryptoStream(ms, transform, CryptoStreamMode.Write))
+                                        {
+                                            byte[] _bytes = Convert.FromBase64String(_reader[index].ToString());
+                                            if (_bytes.Length == 0)
+                                                continue;
+                                            cs.Write(_bytes, 0, _bytes.Length);
+                                            cs.FlushFinalBlock();
+                                            byte[] bytes = (byte[])ms.ToArray();
+                                            objs[index] = Encoding.UTF8.GetString(ms.ToArray());
+                                        }
+                                    }
+                                }
+                                else
+                                    objs[index] = _reader[index];
+                            }
+                            writer.WriteLine(string.Join(",", objs));
+                            rowIndex++;
+                        }
+                        reader.Close();
+                        writer.Close();
+                        return rowIndex;
+                    }
+                }
+            }
+        }
+    }
 }
