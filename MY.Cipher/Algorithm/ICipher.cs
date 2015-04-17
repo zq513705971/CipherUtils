7900286dbdaeb49a85f5adb5a6dd70a8dbe8599f
zhanqun <zhanqun@ibs-tech.com.cn>
2015-04-17 14:47:59 +0800

    update

---
+++ MY.Cipher/Algorithm/ICipher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace MY.Cipher
+{
+    interface ICipher
+    {
+        //string Key { get; set; }
+
+        string Encrypt(string val);
+
+        string Decrypt(string val);
+
+        DataTable Encrypt(DataTable dt, int[] columnIndexs);
+
+        DataTable Decrypt(DataTable dt, int[] columnIndexs);
+
+        int Encrypt(string src, string dest, int[] columns, Predicate<string> action);
+
+        int Decrypt(string src, string dest, int[] columns, Predicate<string> action);
+
+        int Encrypt(string src, string dest, bool hasHeaders, int[] columns, Predicate<string> action);
+
+        int Decrypt(string src, string dest, bool hasHeaders, int[] columns, Predicate<string> action);
+
+        //void GeneralKeyIV(string keyStr, out byte[] key, out byte[] iv);
+    }
+}
---
+++ MY.Cipher/Csv/CsvReader.Enum.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MY.Cipher.Csv
+{
+    [Flags]
+    public enum ValueTrimmingOptions
+    {
+        None = 0,
+        UnquotedOnly = 1,
+        QuotedOnly = 2,
+        All = UnquotedOnly | QuotedOnly
+    }
+
+    /// <summary>
+    /// Specifies the action to take when a parsing error has occured.
+    /// </summary>
+    public enum ParseErrorAction
+    {
+        /// <summary>
+        /// Raises the <see cref="M:CsvReader.ParseError"/> event.
+        /// </summary>
+        RaiseEvent = 0,
+
+        /// <summary>
+        /// Tries to advance to next line.
+        /// </summary>
+        AdvanceToNextLine = 1,
+
+        /// <summary>
+        /// Throws an exception.
+        /// </summary>
+        ThrowException = 2,
+    }
+
+    /// <summary>
+    /// Specifies the action to take when a field is missing.
+    /// </summary>
+    public enum MissingFieldAction
+    {
+        /// <summary>
+        /// Treat as a parsing error.
+        /// </summary>
+        ParseError = 0,
+
+        /// <summary>
+        /// Replaces by an empty value.
+        /// </summary>
+        ReplaceByEmpty = 1,
+
+        /// <summary>
+        /// Replaces by a null value (<see langword="null"/>).
+        /// </summary>
+        ReplaceByNull = 2,
+    }
+
+    /// <summary>
+    /// Defines the data reader validations.
+    /// </summary>
+    [Flags]
+    public enum DataReaderValidations
+    {
+        /// <summary>
+        /// No validation.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Validate that the data reader is initialized.
+        /// </summary>
+        IsInitialized = 1,
+
+        /// <summary>
+        /// Validate that the data reader is not closed.
+        /// </summary>
+        IsNotClosed = 2
+    }
+}
